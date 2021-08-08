2895cf68866164457171bcbf4209a2aa93ccf63c
Drew <ameklund@wisc.edu>
2021-08-08 13:54:22 -0500

    Added queries for retirement info

--- schema.sql
+++ schema.sql
@@ -44,3 +44,12 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+
+CREATE TABLE titles (
+	emp_no int NOT NULL,
+	title varchar NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
